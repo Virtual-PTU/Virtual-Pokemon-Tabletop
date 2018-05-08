60204b24ac1f204450626753f2c21622af77e82c
Tasman Leach <assaultbird2454@gmail.com>
2018-05-08 17:09:38 +1000

    Added contributors file for the website

---
+++ contributors.js
@@ -0,0 +1,79 @@
+/**
+ * When you contribute to Virtual-Pokemon-Tabletop, please add your info to this. You don't need to include `firstName`,
+ * or any of the links if you don't want to (if you don't include something please don't just put in an empty string),
+ * but include everything else.
+ * Note: If you don't pull request into Dev, you won't be put in the credits list (because it uses Dev for data).
+ */
+
+/** 
+ * Valid Tags
+ * -=-=-=-=-=-=-=-=- Dont Use These Tags -=-=-=-=-=-=-=-=-
+ * NOTE: These tags will get applied to you by the Head Developer's and Project Managers...
+ * project-manager      -> User is the project manager
+ * head-developer       -> User is a head developer
+ *
+ *-=-=-=-=-=-=-=-=-=-= Use These Tags =-=-=-=-=-=-=-=-=-=-
+ * developer            -> User contributes code to fix bugs, issues and even features
+ * debug-qa             -> User is testing the software frequently to find bugs
+ * graphics             -> User has provided some graphical assets for use in this project
+ * game-master-advice   -> User has offered advice relating to the PTU system during development.
+ *                         You may add this tag also if your an experienced GM
+ * support              -> User is able to offer technical support with this project
+ */
+initContributors([
+    {
+        username: "AssualtBird2454",
+        firstName: "Tasman",
+
+        shortDescription: "Head dev of VPTU. Started development in mid 2016 and publicised the project in early 2017.",
+
+        tags: [
+            "project-manager",
+            "head-developer",
+            "debug-qa",
+            "support"
+        ],
+        links: {
+            github: "AssualtBird2454",
+            ptForum: "6000923",
+            youtube: "channel/UCXqpxzR-2HYqaHpgtqOAFFg"
+        }
+    }, {
+        username: "zoweb",
+        firstName: "Zach",
+
+        shortDescription: "Casual PTU developer, mostly works on the website.",
+
+        tags: [
+            "developer",
+            "debug-qa",
+            "support"
+        ],
+        links: {
+            github: "zoweb"
+        }
+    }, {
+        username: "Firechild513",
+
+        shortDescription: "Is working on physical pokedex cards for the PTU system, and shared the graphical template for use with the VPTU project.",
+
+        tags: [
+            "graphics"
+        ],
+        links: {
+            ptForum: "6015913"
+        }
+    }, {
+        username: "guswadsworth",
+
+        shortDescription: "Provides support with PTU mechanics and informs developers on how it should be done.",
+
+        tags: [
+            "debug-qa",
+            "game-master-advice"
+        ],
+        links: {
+            ptForum: "4143438"
+        }
+    }
+]);
